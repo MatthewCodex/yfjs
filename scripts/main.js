316cef647fa28d940c2f60c673d4e3526ca54137
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-09-24 12:49:47 -0400

    c

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ battery_lightning.buildType = () => extend(Battery.BatteryBuild, battery_lightni
             damage, 
             posX, 
             posY, 
-            parseFloat(Mathf.random(360.0)),
+            angle,
             length
         );
         }
