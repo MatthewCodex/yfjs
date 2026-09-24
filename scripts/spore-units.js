d9c02668f1fa2b069ec9a1ce472c3c350825a7a1
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-09-24 12:21:42 -0400

    c

--- scripts/spore-units.js
+++ scripts/spore-units.js
@@ -33,34 +33,4 @@ spore_factory.upgrades.add(
 
 
 
-const abc = extend(Wall, "abc", {size: 1, category: Category.untis, buildVisibility: BuildVisibility.shown});
 
-abc.buildType = () => extend(Wall.WallBuild, abc, {
-    onDestroyed: function() {
-        this.super$onDestroyed();
-
-        // 1. Force your numeric values to be strict primitives for Java
-        // 'length' MUST be parsed as an explicit integer or Java rejects the method match
-        let damage = parseFloat(25.0);
-        let posX = parseFloat(this.x);
-        let posY = parseFloat(this.y);
-        let angle = parseFloat(Mathf.random(360.0));
-        let length = parseInt(10); 
-
-        // 2. Safely call the method using a guaranteed color (Color.white)
-        // to ensure an undefined color isn't causing the method match failure
-        const LightningClass = Packages.mindustry.entities.Lightning;
-        
-        LightningClass.create(
-            this.team, 
-            Color.white, 
-            damage, 
-            posX, 
-            posY, 
-            angle, 
-            length
-        );
-
-        Sounds.spark.at(this.x, this.y);
-    }
-});
